45fcb52956f0011e8daed13924846c2629aea6f2
Maurits van Hassel <mcvanhassel@gmail.com>
2020-06-11 17:30:59 +0200

    Add app route configuration with lazy (pre)loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      {
+        path: 'welcome',
+        loadChildren: () => import('./features/welcome/welcome.module').then(m => m.WelcomeModule),
+      },
+      {
+        path: 'search',
+        loadChildren: () => import('./features/search/search.module').then(m => m.SearchModule),
+      },
+      {
+        path: 'not-found',
+        loadChildren: () => import('./features/not-found/not-found.module').then(m => m.NotFoundModule),
+      },
+      { path: '**', redirectTo: 'not-found' },
+    ],
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
