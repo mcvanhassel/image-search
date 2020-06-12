98200f9121ce7b8521a6a2d6b53e9b5eba2ca5dd
Maurits van Hassel <mcvanhassel@gmail.com>
2020-06-12 15:36:48 +0200

    Use hero-section in Welcome component

--- src/app/features/welcome/welcome-feature/welcome-feature.component.ts
+++ src/app/features/welcome/welcome-feature/welcome-feature.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './welcome-feature.component.html',
   styleUrls: ['./welcome-feature.component.scss'],
 })
-export class WelcomeFeatureComponent {}
+export class WelcomeFeatureComponent {
+  heroTitle = 'Welcome to <strong>Gif me Inspiration</strong>';
+  heroText = `
+    How's your mood today? Want to express yourself through pictures?
+    Please allow us to inspire you! :)
+    You'll only have to guide us by typing in some nice words that pop in to your head. We'll do the rest. Promise.
+    Just please make sure that you've entered your GIPHY API Key on our settings page.
+  `;
+}
--- src/app/features/welcome/welcome.module.ts
+++ src/app/features/welcome/welcome.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 
+import { HeroSectionModule } from '../../common/hero-section';
 import { WelcomeFeatureComponent } from './welcome-feature/welcome-feature.component';
 
 @NgModule({
   declarations: [WelcomeFeatureComponent],
-  imports: [CommonModule, MatButtonModule, RouterModule.forChild([{ path: '', component: WelcomeFeatureComponent }])],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: WelcomeFeatureComponent,
+      },
+    ]),
+
+    MatButtonModule,
+    HeroSectionModule,
+  ],
 })
 export class WelcomeModule {}
