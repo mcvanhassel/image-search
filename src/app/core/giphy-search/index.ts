8c9cbc3bd372a04a0a486089a9402f19d7277a7f
Maurits van Hassel <mcvanhassel@gmail.com>
2020-06-12 15:44:30 +0200

    Add barrel file

---
+++ src/app/core/giphy-search/index.ts
@@ -0,0 +1,7 @@
+export { GiphySearchService } from './services/giphy-search.service';
+export { GiphyConfiguration } from './models/giphy-configuration';
+export { GiphySearchModule } from './giphy-search.module';
+export { Giphy } from './models/giphy';
+export { GiphySearchResponse } from './models/giphy-search-response';
+export { Rating } from './models/rating';
+export { GiphySource } from './models/giphy-source';
