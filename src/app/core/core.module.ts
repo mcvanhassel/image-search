18d8a2e0ada93624cc85c1c736c7196efaa63377
Maurits van Hassel <mcvanhassel@gmail.com>
2020-06-12 15:45:33 +0200

    Use GiphySearchModule with config GiphySearchService will now be used for ImageSearchService token

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { NavbarModule } from './navbar/navbar.module';
+import { GiphySearchModule } from './giphy-search';
+import { NavbarModule } from './navbar';
 
 @NgModule({
-  imports: [CommonModule, NavbarModule],
+  imports: [
+    CommonModule,
+    NavbarModule,
+    GiphySearchModule.forRoot({
+      apiUrl: 'https://api.giphy.com/v1',
+      language: 'en',
+    }),
+  ],
   exports: [NavbarModule],
 })
 export class CoreModule {}
