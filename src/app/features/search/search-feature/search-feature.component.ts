1de6c68c3f5f7046f9f5826398e327bab0d8e938
Maurits van Hassel <mcvanhassel@gmail.com>
2020-06-12 15:47:37 +0200

    Clean-up SearchFeature component

--- src/app/features/search/search-feature/search-feature.component.ts
+++ src/app/features/search/search-feature/search-feature.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search-feature',
   templateUrl: './search-feature.component.html',
-  styleUrls: ['./search-feature.component.scss'],
 })
 export class SearchFeatureComponent {}
