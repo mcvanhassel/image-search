a781e8c5faaa8b848854127945d5b391429485f6
Maurits van Hassel <mcvanhassel@gmail.com>
2020-06-13 11:12:28 +0200

    Remove obsolete limit search setting

--- src/app/core/giphy-search/services/giphy-settings.service.ts
+++ src/app/core/giphy-search/services/giphy-settings.service.ts
@@ -9,7 +9,6 @@ import { Rating } from '../models/rating';
 export class GiphySettingsService {
   private readonly apiKeySubject = new BehaviorSubject<string | undefined>(undefined);
   private readonly ratingSubject = new BehaviorSubject<Rating | undefined>(Rating.G);
-  private readonly limitSubject = new BehaviorSubject<number | undefined>(16);
 
   get apiKey(): string | undefined {
     return this.apiKeySubject.value;
@@ -33,17 +32,5 @@ export class GiphySettingsService {
 
   setRating(rating: Rating): void {
     this.ratingSubject.next(rating);
-  }
-
-  get limit(): number | undefined {
-    return this.limitSubject.value;
-  }
-
-  get limit$(): Observable<number | undefined> {
-    return this.limitSubject.asObservable();
-  }
-
-  setLimit(limit: number): void {
-    this.limitSubject.next(limit);
   }
 }
--- src/app/features/search/search-feature/search-settings/search-settings.component.ts
+++ src/app/features/search/search-feature/search-settings/search-settings.component.ts
@@ -14,19 +14,16 @@ export class SearchSettingsComponent {
 
   apiKeyControl = new FormControl(this.settingsService.apiKey, { validators: Validators.required });
   ratingControl = new FormControl(this.settingsService.rating, { validators: Validators.required });
-  limitControl = new FormControl(this.settingsService.limit, { validators: [Validators.required, Validators.min(0), Validators.max(25)] });
 
   formGroup = new FormGroup({
     apiKey: this.apiKeyControl,
     rating: this.ratingControl,
-    limit: this.limitControl,
   });
 
   constructor(private readonly settingsService: GiphySettingsService) {}
 
   saveSettings(): void {
     this.settingsService.setApiKey(this.apiKeyControl.value);
-    this.settingsService.setLimit(this.limitControl.value);
     this.settingsService.setRating(this.ratingControl.value);
     this.formGroup.markAsPristine();
   }
