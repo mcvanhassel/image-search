773a273189ad1ffdd071d8ea86c85cf749579f40
Maurits van Hassel <mcvanhassel@gmail.com>
2020-06-15 21:07:21 +0200

    Implement unit tests for GiphySettings service

--- src/app/core/giphy-search/services/giphy-settings.service.spec.ts
+++ src/app/core/giphy-search/services/giphy-settings.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
+import { Rating } from '../models';
 import { GiphySettingsService } from './giphy-settings.service';
 
 describe('GiphySettingsService', () => {
@@ -12,5 +13,33 @@ describe('GiphySettingsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('apiKey', () => {
+    it('should be undefined by default', () => {
+      expect(service.apiKey).toBeUndefined();
+    });
+
+    it('should be assigned and emitted', () => {
+      service.setApiKey('123');
+
+      const expected: string = '123';
+      expect(service.apiKey).toBe(expected);
+      service.apiKey$.subscribe(apiKey => expect(apiKey).toBe(expected));
+    });
+  });
+
+  describe('rating', () => {
+    it('should be "G" by default', () => {
+      expect(service.rating).toBe(Rating.G);
+    });
+
+    it('should be assigned and emitted', () => {
+      service.setRating(Rating.PG13);
+
+      const expected: Rating = Rating.PG13;
+      expect(service.rating).toBe(expected);
+      service.rating$.subscribe(rating => expect(rating).toBe(expected));
+    });
   });
 });
