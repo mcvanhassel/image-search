ae6886f89f36a949ea6a63746a5b3f4ec5640cc9
Maurits van Hassel <mcvanhassel@gmail.com>
2020-06-15 23:49:58 +0200

    Fix unit test console warnings for WelcomeFeature component

--- src/app/features/welcome/welcome-feature/welcome-feature.component.spec.ts
+++ src/app/features/welcome/welcome-feature/welcome-feature.component.spec.ts
@@ -1,5 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { HeroSectionModule } from '../../../common/hero-section';
 import { WelcomeFeatureComponent } from './welcome-feature.component';
 
 describe('WelcomeFeatureComponent', () => {
@@ -8,6 +11,7 @@ describe('WelcomeFeatureComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule.withRoutes([]), MatButtonModule, HeroSectionModule],
       declarations: [WelcomeFeatureComponent],
     }).compileComponents();
   }));
