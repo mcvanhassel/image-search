9fd6fa0c03a90c837db92b01287c4b3564ca4c10
Maurits van Hassel <mcvanhassel@gmail.com>
2020-06-15 10:46:20 +0200

    Implement unit tests for ApiKey guard

--- src/app/core/giphy-search/guards/api-key.guard.spec.ts
+++ src/app/core/giphy-search/guards/api-key.guard.spec.ts
@@ -1,19 +1,42 @@
 import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
+import { GiphySettingsService } from '../services/giphy-settings.service';
 import { ApiKeyGuard } from './api-key.guard';
+
+class TestComponent {}
 
 describe('ApiKeyGuard', () => {
   let guard: ApiKeyGuard;
+  let router: Router;
+  let imageSettingsService: GiphySettingsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes([])],
+      imports: [RouterTestingModule.withRoutes([{ path: 'search/settings', component: TestComponent }])],
     });
+
     guard = TestBed.inject(ApiKeyGuard);
+    router = TestBed.inject(Router);
+    imageSettingsService = TestBed.inject(GiphySettingsService);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  it('should continue navigation when api key is set', () => {
+    spyOnProperty(imageSettingsService, 'apiKey$').and.returnValue(of('0123456789'));
+
+    guard.canActivate().subscribe(canActivate => expect(canActivate).toBe(true));
+  });
+
+  it('should redirect route when api key is not set', () => {
+    spyOn(router, 'parseUrl');
+    spyOnProperty(imageSettingsService, 'apiKey$').and.returnValue(of(null));
+
+    guard.canActivate().subscribe(() => expect(router.parseUrl).toHaveBeenCalledWith('/search/settings'));
   });
 });
