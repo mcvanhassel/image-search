9e4df044fa1a1e17bf5e0c61c209e3fa400aa84f
Maurits van Hassel <mcvanhassel@gmail.com>
2020-06-15 09:23:44 +0200

    ImageSearchServiceToken should be typed with ImageSearchService

--- src/app/core/image-search/image-search.service.ts
+++ src/app/core/image-search/image-search.service.ts
@@ -1,9 +1,9 @@
-import { InjectionToken, Provider } from '@angular/core';
+import { InjectionToken } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { ImageSearchResponse } from './image-search-response';
 
-export const ImageSearchServiceToken = new InjectionToken<Provider>('ImageSearchServiceToken');
+export const ImageSearchServiceToken = new InjectionToken<ImageSearchService>('ImageSearchServiceToken');
 
 export interface ImageSearchService {
   search(query: string, pageIndex?: number, pageSize?: number): Observable<ImageSearchResponse>;
