7c316588552e417ac0b358e07a2229df897ff607
Maurits van Hassel <mcvanhassel@gmail.com>
2020-06-16 12:39:43 +0200

    Fix unit test console warnings

--- src/app/features/search/search-feature/search-image/search-image.component.spec.ts
+++ src/app/features/search/search-feature/search-image/search-image.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { of } from 'rxjs';
 
+import { HeroSectionModule } from '../../../../common/hero-section';
 import { CensoredWordsToken } from '../../../../core/censored-words';
 import { ImageSearchService, ImageSearchServiceToken } from '../../../../core/image-search';
 import { SearchImageComponent } from './search-image.component';
@@ -17,6 +23,7 @@ describe('SearchImageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatPaginatorModule, HeroSectionModule],
       declarations: [SearchImageComponent],
       providers: [
         { provide: ImageSearchServiceToken, useClass: MockImageSearchService },
--- src/app/features/search/search-feature/search-settings/search-settings.component.spec.ts
+++ src/app/features/search/search-feature/search-settings/search-settings.component.spec.ts
@@ -1,5 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HeroSectionModule } from '../../../../common/hero-section';
 import { SearchSettingsComponent } from './search-settings.component';
 
 describe('SearchSettingsComponent', () => {
@@ -8,6 +16,19 @@ describe('SearchSettingsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        NoopAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatCardModule,
+        MatSelectModule,
+
+        HeroSectionModule,
+      ],
       declarations: [SearchSettingsComponent],
     }).compileComponents();
   }));
