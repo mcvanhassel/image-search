8eeb99991256a85f83af2ac5530f32366f174db7
Maurits van Hassel <mcvanhassel@gmail.com>
2020-06-17 20:13:01 +0200

    Fix sonarcloud code smells

--- src/app/core/image-search/image-source.ts
+++ src/app/core/image-search/image-source.ts
@@ -2,8 +2,8 @@ import { SafeUrl } from '@angular/platform-browser';
 
 export interface ImageSource {
   url: SafeUrl | undefined;
-  mp4?: SafeUrl | undefined;
-  webp?: SafeUrl | undefined;
+  mp4: SafeUrl | undefined;
+  webp: SafeUrl | undefined;
   height: number;
   width: number;
 }
